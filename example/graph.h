f9a4b1668278c604ffa0cad4aeb4ee31e3394d83
ousttrue <ousttrue@gmail.com>
2020-07-10 15:31:00 +0900

    method

--- example/graph.h
+++ example/graph.h
@@ -185,22 +185,119 @@ public:
 
     // Element access
 
-    NodeType& node(int node_id);
-    const NodeType& node(int node_id) const;
-    Span<const int> neighbors(int node_id) const;
-    Span<const Edge> edges() const;
+    NodeType& node(const int id)
+    {
+        return const_cast<NodeType&>(static_cast<const Graph*>(this)->node(id));
+    }
+    const NodeType& node(const int id) const
+    {
+        const auto iter = nodes_.find(id);
+        assert(iter != nodes_.end());
+        return *iter;
+    }
+
+    Span<const int> neighbors(int node_id) const
+    {
+        const auto iter = node_neighbors_.find(node_id);
+        assert(iter != node_neighbors_.end());
+        return *iter;
+    }
+    Span<const typename Edge> edges() const
+    {
+        return edges_.elements();
+    }
 
     // Capacity
 
-    size_t num_edges_from_node(int node_id) const;
+    size_t num_edges_from_node(const int id) const
+    {
+        auto iter = edges_from_node_.find(id);
+        assert(iter != edges_from_node_.end());
+        return *iter;
+    }
 
     // Modifiers
 
-    int insert_node(const NodeType& node);
-    void erase_node(int node_id);
+    int insert_node(const NodeType& node)
+    {
+        const int id = current_id_++;
+        assert(!nodes_.contains(id));
+        nodes_.insert(id, node);
+        edges_from_node_.insert(id, 0);
+        node_neighbors_.insert(id, std::vector<int>());
+        return id;
+    }
 
-    int insert_edge(int from, int to);
-    void erase_edge(int edge_id);
+    void erase_node(const int id)
+    {
+
+        // first, remove any potential dangling edges
+        {
+            static std::vector<int> edges_to_erase;
+
+            for (const Edge& edge : edges_.elements())
+            {
+                if (edge.contains(id))
+                {
+                    edges_to_erase.push_back(edge.id);
+                }
+            }
+
+            for (const int edge_id : edges_to_erase)
+            {
+                erase_edge(edge_id);
+            }
+
+            edges_to_erase.clear();
+        }
+
+        nodes_.erase(id);
+        edges_from_node_.erase(id);
+        node_neighbors_.erase(id);
+    }
+
+    int insert_edge(const int from, const int to)
+    {
+        const int id = current_id_++;
+        assert(!edges_.contains(id));
+        assert(nodes_.contains(from));
+        assert(nodes_.contains(to));
+        edges_.insert(id, Edge(id, from, to));
+
+        // update neighbor count
+        assert(edges_from_node_.contains(from));
+        *edges_from_node_.find(from) += 1;
+        // update neighbor list
+        assert(node_neighbors_.contains(from));
+        node_neighbors_.find(from)->push_back(to);
+
+        return id;
+    }
+
+    void erase_edge(const int edge_id)
+    {
+        // This is a bit lazy, we find the pointer here, but we refind it when we erase the edge
+        // based on id key.
+        assert(edges_.contains(edge_id));
+        const Edge& edge = *edges_.find(edge_id);
+
+        // update neighbor count
+        assert(edges_from_node_.contains(edge.from));
+        int& edge_count = *edges_from_node_.find(edge.from);
+        assert(edge_count > 0);
+        edge_count -= 1;
+
+        // update neighbor list
+        {
+            assert(node_neighbors_.contains(edge.from));
+            auto neighbors = node_neighbors_.find(edge.from);
+            auto iter = std::find(neighbors->begin(), neighbors->end(), edge.to);
+            assert(iter != neighbors->end());
+            neighbors->erase(iter);
+        }
+
+        edges_.erase(edge_id);
+    }
 
 private:
     int current_id_;
@@ -212,127 +309,6 @@ private:
     // This container maps to the edge id
     IdMap<Edge> edges_;
 };
-
-template<typename NodeType>
-NodeType& Graph<NodeType>::node(const int id)
-{
-    return const_cast<NodeType&>(static_cast<const Graph*>(this)->node(id));
-}
-
-template<typename NodeType>
-const NodeType& Graph<NodeType>::node(const int id) const
-{
-    const auto iter = nodes_.find(id);
-    assert(iter != nodes_.end());
-    return *iter;
-}
-
-template<typename NodeType>
-Span<const int> Graph<NodeType>::neighbors(int node_id) const
-{
-    const auto iter = node_neighbors_.find(node_id);
-    assert(iter != node_neighbors_.end());
-    return *iter;
-}
-
-template<typename NodeType>
-Span<const typename Graph<NodeType>::Edge> Graph<NodeType>::edges() const
-{
-    return edges_.elements();
-}
-
-template<typename NodeType>
-size_t Graph<NodeType>::num_edges_from_node(const int id) const
-{
-    auto iter = edges_from_node_.find(id);
-    assert(iter != edges_from_node_.end());
-    return *iter;
-}
-
-template<typename NodeType>
-int Graph<NodeType>::insert_node(const NodeType& node)
-{
-    const int id = current_id_++;
-    assert(!nodes_.contains(id));
-    nodes_.insert(id, node);
-    edges_from_node_.insert(id, 0);
-    node_neighbors_.insert(id, std::vector<int>());
-    return id;
-}
-
-template<typename NodeType>
-void Graph<NodeType>::erase_node(const int id)
-{
-
-    // first, remove any potential dangling edges
-    {
-        static std::vector<int> edges_to_erase;
-
-        for (const Edge& edge : edges_.elements())
-        {
-            if (edge.contains(id))
-            {
-                edges_to_erase.push_back(edge.id);
-            }
-        }
-
-        for (const int edge_id : edges_to_erase)
-        {
-            erase_edge(edge_id);
-        }
-
-        edges_to_erase.clear();
-    }
-
-    nodes_.erase(id);
-    edges_from_node_.erase(id);
-    node_neighbors_.erase(id);
-}
-
-template<typename NodeType>
-int Graph<NodeType>::insert_edge(const int from, const int to)
-{
-    const int id = current_id_++;
-    assert(!edges_.contains(id));
-    assert(nodes_.contains(from));
-    assert(nodes_.contains(to));
-    edges_.insert(id, Edge(id, from, to));
-
-    // update neighbor count
-    assert(edges_from_node_.contains(from));
-    *edges_from_node_.find(from) += 1;
-    // update neighbor list
-    assert(node_neighbors_.contains(from));
-    node_neighbors_.find(from)->push_back(to);
-
-    return id;
-}
-
-template<typename NodeType>
-void Graph<NodeType>::erase_edge(const int edge_id)
-{
-    // This is a bit lazy, we find the pointer here, but we refind it when we erase the edge based
-    // on id key.
-    assert(edges_.contains(edge_id));
-    const Edge& edge = *edges_.find(edge_id);
-
-    // update neighbor count
-    assert(edges_from_node_.contains(edge.from));
-    int& edge_count = *edges_from_node_.find(edge.from);
-    assert(edge_count > 0);
-    edge_count -= 1;
-
-    // update neighbor list
-    {
-        assert(node_neighbors_.contains(edge.from));
-        auto neighbors = node_neighbors_.find(edge.from);
-        auto iter = std::find(neighbors->begin(), neighbors->end(), edge.to);
-        assert(iter != neighbors->end());
-        neighbors->erase(iter);
-    }
-
-    edges_.erase(edge_id);
-}
 
 template<typename NodeType, typename Visitor>
 void dfs_traverse(const Graph<NodeType>& graph, const int start_node, Visitor visitor)
